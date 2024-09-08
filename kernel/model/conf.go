7cc40e943002df1f951a337a4343b31c0e45cc5e
unknown <aa@example.com>
2024-09-08 21:46:36 +0800

    update

--- kernel/model/conf.go
+++ kernel/model/conf.go
@@ -837,7 +837,8 @@ func InitBoxes() {
 
 func IsSubscriber() bool {
 	u := Conf.GetUser()
-	return nil != u && (-1 == u.UserSiYuanProExpireTime || 0 < u.UserSiYuanProExpireTime) && 0 == u.UserSiYuanSubscriptionStatus
+	// return nil != u && (-1 == u.UserSiYuanProExpireTime || 0 < u.UserSiYuanProExpireTime) && 0 == u.UserSiYuanSubscriptionStatus
+	return true
 }
 
 func IsPaidUser() bool {
